03189c97eec412921c6e47373e0f4ba76a1098b8
raf952 <Ralph.Foy@objectPrime.com>
2014-05-10 01:23:33 -0500

    almost done...

--- Worksheet.R
+++ Worksheet.R
@@ -11,22 +11,32 @@ loadHpcData <- function () {
         hpc.data[hpc.data$Date=="2007-02-01"|hpc.data$Date=="2007-02-02" ,]
 }
 gap.label <-"Global Active Power (kilowats)"
-plot1 <- function(){
-   hist(hpc.data$Global_active_power, col="red", main="Global Active Power", xlab=gap.label, ylab="Frequency")
+dev.on <- function(name) {
+        png(name, width=480, height=480, units="px" )
+}
+plot1 <- function(fname = NULL){
+        if (!is.null(fname)) dev.on(fname)
+        hist(hpc.data$Global_active_power, col="red", main="Global Active Power", xlab=gap.label, ylab="Frequency")
+        if (!is.null(fname)) dev.off()   
 }
 
-plot2 <- function(){
+
+plot2 <- function(fname = NULL){
+        if (!is.null(fname)) dev.on(fname)
         plot(hpc.data$Time, hpc.data$Global_active_power, type="l"
              , ylab=gap.label, xlab="")
+        if (!is.null(fname)) dev.off()   
 }
 
 esm.label<-"Energy sub meeting"
-plot3 <- function(){
+plot3 <- function(fname = NULL){
+        if (!is.null(fname)) {dev.on(fname) ; lwd = 1} else {lwd = 0}
         plot(hpc.data$Time, hpc.data$Sub_metering_1, type="l", ylab=esm.label, xlab="")
         lines(hpc.data$Time,hpc.data$Sub_metering_2, col="red" )
         lines(hpc.data$Time,hpc.data$Sub_metering_3, col="blue" )
         legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty = c(1,1,1)
-               , col=c('black','red','blue'), box.col=NA)
+               , col=c('black','red','blue'), box.lwd=lwd )
+        if (!is.null(fname)) dev.off()   
 }
 grp.label <-"Global Reactive Power (kilowats)"
 
@@ -38,10 +48,14 @@ plot4a <- function(){
         plot(hpc.data$Time, hpc.data$Voltage, type="l", ylab="Voltage", xlab="")
 }
 
-plot4 <- function() {
+plot4 <- function(fname = NULL) {
+        if (!is.null(fname)) dev.on(fname)
         par(mfcol=c(2,2))
         plot2()
         plot3()
         plot4a()
         plot4b()
+        if (!is.null(fname)) dev.off()   
+        par(mfcol=c(1,1))
+        
 }
